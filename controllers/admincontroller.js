bca9a170af09fd87720365ce52d7a25eab3f2ebc
juanmexico <alimexico021@gmail.com>
2025-02-09 16:56:18 +0330

    jalali-moment

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -1,4 +1,5 @@
 const Blog = require("../models/blog");
+const { formatDate } = require("../utils/jalali");
 
 exports.getDashboard = async (req, res) => {
   try {
@@ -9,6 +10,7 @@ exports.getDashboard = async (req, res) => {
       layout: "./layouts/dashLayout",
       fullname: req.user.fullname,
       blogs,
+      formatDate,
     });
   } catch (err) {
     console.log(err);
